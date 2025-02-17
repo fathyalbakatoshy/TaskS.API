a94aa190e6cbbef8a42dd4b3e337a739be5c85e8
Fathi Mostafa <tetomostafa20141@gmail.com>
2025-02-17 23:21:48 +0200

    Add project files.

---
+++ Task.Repository/Data/TaskContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Core.Entities;
+
+namespace Task.Repository.Data
+{
+    public class TaskContext : DbContext
+    {
+
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
